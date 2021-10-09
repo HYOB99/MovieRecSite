14e4e16cd5e72d2e4c4afa87ee1a5929d306aadb
hakyung <hakyung4@illinois.edu>
2021-10-08 23:37:41 -0500

    add changes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import SignupForm, SignInForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c4ebd5c19784795cce458b28cf63e20a' #Used Secrets module in python to generate a secret key to protect against some potential attacks. 
 
 posts = [
     {
@@ -27,3 +30,17 @@ def recent_movies_page():
 @app.route("/genres")
 def genres_page():
     return render_template('genres.html', title='Genres')
+
+@app.route("/sign_in")
+def sign_in_page():
+    form = SignInForm()
+    return render_template('sign_in.html', title='Sign in', form=form)
+
+@app.route("/sign_up")
+def sign_up_page():
+    form = SignupForm()
+    return render_template('sign_up.html', title='Sign up', form=form)
+
+@app.route("/movie_picker")
+def movie_picker_page():
+    return render_template('movie_picker.html', title='Movie Picker')
