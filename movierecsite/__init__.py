7ff3e17877ced5ef5d51823ace553afb99051ed2
hakyung <hakyung4@illinois.edu>
2021-11-23 16:57:32 -0600

    start building basic login and logout structure

--- movierecsite/__init__.py
+++ movierecsite/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c4ebd5c19784795cce458b28cf63e20a' #Used Secrets module in python to generate a secret key to protect against some potential attacks. 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #specifying the relative path to the current file
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'sign_in_page'
 
 from movierecsite import routes
--- movierecsite/routes.py
+++ movierecsite/routes.py
@@ -1,11 +1,9 @@
-from flask import render_template, url_for, flash, redirect
-from movierecsite import app
+from flask import render_template, url_for, flash, redirect, request
+from movierecsite import app, db, bcrypt
 from movierecsite.forms import SignupForm, SignInForm
-from flask_bcrypt import Bcrypt
 from movierecsite.models import User, Post
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+from flask_login import login_user, current_user, logout_user, login_required
+
 
 @app.route("/")
 @app.route("/home")
@@ -26,17 +24,39 @@ def genres_page():
 
 @app.route("/sign_in", methods=['GET', 'POST'])
 def sign_in_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('main_page'))
     form = SignInForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next') #This is to simply redirect to a page that a user was trying to access before logging in.
+            return redirect(next_page) if next_page else redirect(url_for('main_page')) #redirects to the next page if it exists. Otherwise, go to home page.
+        else:
+            flash('Login Failed. Please check email and paassword', 'danger')
     return render_template('sign_in.html', title='Sign In', form=form)
 
 @app.route("/sign_up", methods=['GET', 'POST'])
 def sign_up_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('main_page'))
     form = SignupForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8') #hashing password
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8') #hashing password. Using .decode('utf-8') to make it a string instead of binary.
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user) #add user to the database
         db.session.commit() #commit changes to database
         flash('Your account has been created!', 'sucess')
         return redirect(url_for('sign_in_page'))
     return render_template('sign_up.html', title='Sign Up', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('main_page'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
